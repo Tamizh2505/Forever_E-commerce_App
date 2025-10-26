c6f0abb02b96a6731656a8180e6e46ff6eb305ce
Tamizh2505 <tamilselvan123252000@gmail.com>
2025-10-26 10:04:11 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:process.env.VITE_BASE_PATH || '/Forever_E-commerce'
-})
+  base: process.env.BASE_PATH || '/',
+  // root is default (project root) so no need to set it explicitly
+  build: {
+    outDir: 'dist', // default, must match Vercel output
+  },
+});
